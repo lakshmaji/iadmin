0b1fb9eae206272e999ba80ca64eeeccd1fd1ac5
MUTYALA LAKSHMAJI <lakshmaji@users.noreply.github.com>
2019-02-10 22:06:34 +0530

    create query update

--- create/crtinstbl.php
+++ create/crtinstbl.php
@@ -4,9 +4,11 @@ session_start();
 include('../connection.php');
 $connection = new createConnection(); 			//created a new object
 $connection_ref = $connection->connectToDatabase();
+// $connection->selectDatabase();				//selecting db
 
 $num_fields=$_SESSION['num_flds'];
 $selected_table_name=$_SESSION["tblname"];
+//echo $selected_table_name."<br/>";
 
 
 
@@ -21,6 +23,7 @@ for($y=1;$y<$num_fields;$y++)
 $str=$str.");";
 
 $re_result = mysqli_query ($connection_ref, $str); //run the query
+
 echo "<script>
 		var r = confirm('ADDED NEW ENTRY SUCCESSFULLY!Do You Want To Add One More?');
     		if (r == true) 
