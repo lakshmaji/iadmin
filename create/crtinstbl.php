ead9c509b23234ef73f6e6c53d9f14173b360e1e
MUTYALA LAKSHMAJI <lakshmaji@users.noreply.github.com>
2019-02-10 22:05:47 +0530

    insert sql update

--- create/crtinstbl.php
+++ create/crtinstbl.php
@@ -4,15 +4,13 @@ session_start();
 include('../connection.php');
 $connection = new createConnection(); 			//created a new object
 $connection_ref = $connection->connectToDatabase();
-// $connection->selectDatabase();				//selecting db
 
 $num_fields=$_SESSION['num_flds'];
 $selected_table_name=$_SESSION["tblname"];
-//echo $selected_table_name."<br/>";
 
 
 
-$str="INSERT INTO ".$selected_table_name." VALUES(''";
+$str="INSERT INTO ".$selected_table_name." VALUES(null";
 
 
 for($y=1;$y<$num_fields;$y++)
@@ -23,10 +21,6 @@ for($y=1;$y<$num_fields;$y++)
 $str=$str.");";
 
 $re_result = mysqli_query ($connection_ref, $str); //run the query
-
-
-//echo "inserted successfully";
-
 echo "<script>
 		var r = confirm('ADDED NEW ENTRY SUCCESSFULLY!Do You Want To Add One More?');
     		if (r == true) 
