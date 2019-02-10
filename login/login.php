ccad3d84751128233de25294144712e90d5c14e2
MUTYALA LAKSHMAJI <lakshmaji@users.noreply.github.com>
2019-02-10 22:09:05 +0530

    login mysqli changes

--- login/login.php
+++ login/login.php
@@ -4,7 +4,6 @@
 							include('../connection.php');
 						$connection = new createConnection(); 			//created a new object
 						$connection_ref = $connection->connectToDatabase();
-						// $connection->selectDatabase();
 
 
 	if (isset($_POST['submit'])) 
@@ -20,7 +19,6 @@
 			$password=$_POST['password'];
 			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 			
-//$connection = mysql_connect("localhost", "root", "");
 			// To protect MySQL injection for Security purpose
 			$username = stripslashes($username);
 			$password = stripslashes($password);
@@ -28,11 +26,9 @@
 			$password = md5(mysqli_real_escape_string($connection_ref,$password));
 			// Selecting Database
 			
-//$db = mysql_select_db("db_s_vv", $connection);
 			// SQL query to fetch information of registerd users and finds user match.
-			$query = mysqli_query($connection_ref, "select * from admin_log where password='$password' AND username='$username' ")or die(mysql_error());
+			$query = mysqli_query($connection_ref, "select * from admin_log where password='$password' AND username='$username' ")or die(mysqli_error($connection_ref));
 			$rows = mysqli_num_rows($query);
-			// echo $rows;die();
 			if ($rows == 1) 
 			{
 				$_SESSION['login_user']=$username; // Initializing Session
@@ -43,7 +39,7 @@
 				//Username or Password is invalid";
 				header("Location: index.php"); // Redirecting To Other Page		
 			}
-			mysql_close($connection); // Closing Connection
+			mysqli_close($connection); // Closing Connection
 		}
 	}
 ?>
